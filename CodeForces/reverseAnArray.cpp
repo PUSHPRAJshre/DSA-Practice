7ce7455edaf11291ff4c6b54cdf46c121daba3e6
Pushapraj Singh <pushapraj.cool05@gmail.com>
2026-03-28 20:25:58 +0530

    Added DSA practice folders

---
+++ CodeForces/reverseAnArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int j = n - 1;
+    for(int i = 0; i<n; i++){
+        if(j>i){
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+            j--;
+        }
+    }
+
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
